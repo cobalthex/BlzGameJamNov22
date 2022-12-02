f44bff6f22359b2f07bf4fab119434e8e003b9af
Glen Chang <glen.r.chang@gmail.com>
2022-12-01 21:05:37 -0800

    Add Driveway Generator and World_1 scene

---
+++ YellowSnowball/Assets/Code/Driveway.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driveway : MonoBehaviour
+{
+    private int m_dimensionX;
+    public int DimensionX => m_dimensionX;
+    
+    private int m_dimensionZ;
+    public int DimensionZ => m_dimensionZ;
+
+    [SerializeField]
+    private GameObject m_cellObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hide proxy driveway
+        gameObject.HideRenderers();
+
+        // Get dimension from scale
+        var localScale = transform.localScale;
+        m_dimensionX = (int)localScale.x;
+        m_dimensionZ = (int)localScale.z;
+
+        // Draw proxy grid
+        var drivewayPos = transform.position;
+        for (int x = 0; x < DimensionX; x++)
+        {
+            for (int z = 0; z < DimensionZ; z++)
+            {
+                var cell = Instantiate(m_cellObject, new Vector3(x, 0f, z) + drivewayPos, Quaternion.identity);
+                cell.transform.SetParent(transform);
+            }
+        }
+    }
+}
